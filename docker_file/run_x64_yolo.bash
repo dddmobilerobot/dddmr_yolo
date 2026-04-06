a514941399540c59a217b0c6689e1c8045559f15
apola <yan_1whun1@hotmail.com>
2026-04-06 09:47:06 +0400

    update script

--- docker_file/run_x64_yolo.bash
+++ docker_file/run_x64_yolo.bash
@@ -2,11 +2,12 @@
 
 xhost +local:docker
 
-is_yolo=$(docker image ls dddmr | grep yolo | cut -c14-17)
-is_yolo_l4t_r36=$(docker image ls dddmr | grep yolo_l4t_r36 | cut -c7-18)
+LS_IMAGE=$(docker image ls dddmr)
+YOLO='yolo'
+YOLO_L4T='yolo_l4t_r36'
 
-if [[ $is_yolo_l4t_r36 == "yolo_l4t_r36" ]]; then
-    echo "Detect image of dddmr:"$is_yolo_l4t_r36
+if echo "$LS_IMAGE" | grep -q "$YOLO_L4T"; then
+    echo "Detect image of dddmr:"$YOLO_L4T
     docker run -it \
         --privileged \
         --network=host \
@@ -22,8 +23,8 @@ if [[ $is_yolo_l4t_r36 == "yolo_l4t_r36" ]]; then
         --name="dddmr_yolo_train2onnx" \
         dddmr:yolo_l4t_r36
 
-elif [[ $is_yolo == "yolo" ]]; then
-    echo "Detect image of dddmr:"$is_yolo
+elif echo "$LS_IMAGE" | grep -q "$YOLO"; then
+    echo "Detect image of dddmr:"$YOLO
     docker run -it \
         --privileged \
         --network=host \
